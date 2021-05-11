f092ca482c17b5f85d7eab25441da40a0c32a9eb
Miguel Cabeza <miguel567@gmail.com>
2021-05-12 00:06:10 +0200

    fetching address for 1 vault

--- holders.py
+++ holders.py
@@ -2,6 +2,11 @@ import requests
 import json
 
 
+def getTokens():
+    allVaults = requests.get("https://vaults.finance/all")
+    
+    print(json.dumps(allVaults.json()[0]["token"]["address"]))
+
 def main():
     params = {
         'token':'0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
@@ -13,7 +18,7 @@ def main():
     response = requests.get("https://api.bloxy.info/token/token_holders_list", params=params)
     f.write(json.dumps(response.json()))
     f.close()
-    """ print(json.dumps(response.json())) """
+    
 
 
-main()
+getTokens()
