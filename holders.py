0143b05e3fee3e5dc7c60a67dbc182d31b1a77d9
Miguel Cabeza <miguel567@gmail.com>
2021-05-11 23:39:47 +0200

    wip

--- holders.py
+++ holders.py
@@ -1,9 +1,19 @@
 import requests
+import json
 
 
 def main():
-    response = requests.get("https://api.bloxy.info/token/token_holders_list?token=0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e&key=ACCXw398IJc99&format=table")
-    print(response.json())
+    params = {
+        'token':'0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+        'key': 'ACCXw398IJc99',
+        'format': 'json'
+    }
+
+    f = open(params['token']+".json", "w")
+    response = requests.get("https://api.bloxy.info/token/token_holders_list", params=params)
+    f.write(json.dumps(response.json()))
+    f.close()
+    """ print(json.dumps(response.json())) """
 
 
 main()
